cd7f3b10f790473e1fc5e676791d93cdc5261b9d
Sourcery AI <>
2023-01-22 21:16:30 +0000

    'Refactored by Sourcery'

--- buildroot/share/PlatformIO/scripts/common-dependencies-post.py
+++ buildroot/share/PlatformIO/scripts/common-dependencies-post.py
@@ -12,9 +12,9 @@ if "idedata" in COMMAND_LINE_TARGETS:
 Import("projenv")
 
 def apply_board_build_flags():
-	if not 'BOARD_CUSTOM_BUILD_FLAGS' in env['MARLIN_FEATURES']:
-		return
-	projenv.Append(CCFLAGS=env['MARLIN_FEATURES']['BOARD_CUSTOM_BUILD_FLAGS'].split())
+    if 'BOARD_CUSTOM_BUILD_FLAGS' not in env['MARLIN_FEATURES']:
+        return
+    projenv.Append(CCFLAGS=env['MARLIN_FEATURES']['BOARD_CUSTOM_BUILD_FLAGS'].split())
 
 # We need to add the board build flags in a post script
 # so the platform build script doesn't overwrite the custom CCFLAGS
